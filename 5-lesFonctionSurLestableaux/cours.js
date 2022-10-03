0d6fd845f70f1ef4500039da2f18d4d476776ab0
hatim77100 <najoua-hatim@hotmail.fr>
2022-10-03 22:44:57 +0200

    update function array

---
+++ 5-lesFonctionSurLestableaux/cours.js
@@ -0,0 +1,30 @@
+const notes = [5, 10, 15, 20];
+
+// supprimer un element à la fin du tableau
+notes.pop()
+// console.log(notes);
+
+// rajouter un élement ou plusieur à la fin du tableau
+notes.push(25)
+// console.log(notes);
+
+// la fonction Map qui récupère un tableau et apporte une modification sur celui-ci en retournant un nouveau tableau.
+const notes2 = notes.map(function(note) {
+    // return Math.pow(note,2)
+    return note ** 2;
+});
+// console.log(notes2);
+
+// la fonction findIndex(): 
+const index = notes.findIndex(function(note) {
+    return note === 15;
+})
+// console.log(index);
+
+// La fonction find()
+const persos = [
+    ["Tya",5, 10, 15, 24]
+    ["Toto",8, 15, 12, 25]
+    ["Milo",9, 11, 7, 28]
+];
+
